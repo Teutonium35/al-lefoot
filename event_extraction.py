fd508daab623bf840e95730aff2c96133f360b76
MathieuH35 <teutonium35@gmail.com>
2024-11-05 10:40:54 +0100

    feat: add match list extraction

--- event_extraction.py
+++ event_extraction.py
@@ -5,10 +5,15 @@ competition_id = 7
 season_id = 27
 
 match_path = Path(__file__).parent.parent / 'open-data' / 'data' / 'matches' / f'{competition_id}' / f'{season_id}.json'
+match_path = Path(__file__).parent.parent / 'temp.json'
 
-print(f'{match_path=}')
+# print(f'{match_path=}')
 
 with open(match_path) as match_file:
     match_data = loads(match_file.read())
 
-print(f'{match_data=}')
+# print(f'{match_data=}')
+
+match_list = [match_details['match_id'] for match_details in match_data]
+
+print(match_list)
