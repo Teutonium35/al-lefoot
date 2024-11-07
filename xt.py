aef3b2d6784143f9b0aa7f91c692842aa17bc07d
MathieuH35 <teutonium35@gmail.com>
2024-11-07 23:49:02 +0100

    feat: add xt matrix construction for single match

--- xt.py
+++ xt.py
@@ -65,14 +65,21 @@ def get_position_from_shot(shot_event) -> Tuple[int, int]:
     shot_position = shot_event["location"]
     return shot_position[0], shot_position[1]
 
-def generate_base_matrix_pass():
-    return [[0]*LENGTH_SUBDIVISION_AMOUNT*WIDTH_SUBDIVISION_AMOUNT]*LENGTH_SUBDIVISION_AMOUNT*WIDTH_SUBDIVISION_AMOUNT
+def generate_base_matrix_3d():
+    matrix = [[0]*LENGTH_SUBDIVISION_AMOUNT*WIDTH_SUBDIVISION_AMOUNT for _ in range(LENGTH_SUBDIVISION_AMOUNT*WIDTH_SUBDIVISION_AMOUNT)]
+    print(f'{matrix=}')
+    matrix[0][0] += 1
+    print(f'{matrix=}')
+    return matrix
 
-def generate_base_matrix_shot():
+def generate_base_matrix_2d():
     return [0]*LENGTH_SUBDIVISION_AMOUNT*WIDTH_SUBDIVISION_AMOUNT
 
 def generate_movement_matrix(pass_events, carry_events):
-    matrix = generate_base_matrix_pass()
+    matrix = generate_base_matrix_3d()
+    # print(f'{matrix=}')
+    # print(f'{len(matrix)=}')
+    # print(f'{len(matrix[0])=}')
 
     for pass_event in pass_events:
         start_x, start_y, end_x, end_y = get_positions_from_pass(pass_event)
@@ -81,6 +88,9 @@ def generate_movement_matrix(pass_events, carry_events):
         start_array_index = get_array_position_from_field_indexes(start_lindex, start_windex)
         end_array_index = get_array_position_from_field_indexes(end_lindex, end_windex)
         matrix[start_array_index][end_array_index] += 1
+        print(f'{matrix[start_array_index][end_array_index]=}')
+        print(f'{start_array_index=}')
+        print(f'{end_array_index=}')
     # print(f'{matrix=}')
 
     for carry_event in carry_events:
@@ -90,13 +100,40 @@ def generate_movement_matrix(pass_events, carry_events):
         start_array_index = get_array_position_from_field_indexes(start_lindex, start_windex)
         end_array_index = get_array_position_from_field_indexes(end_lindex, end_windex)
         matrix[start_array_index][end_array_index] += 1
-    # print(f'{matrix=}')
 
+    print(f'{matrix[0]=}')
+    test = 0
+    for index in matrix[0]:
+        test += matrix[0][index]
+    print(f'{test=}')
+    print(f'{matrix[45]=}')
+    matrix[0][0] +=1
+
+    print(f'{matrix[0]=}')
+    test = 0
+    for index in matrix[0]:
+        test += matrix[0][index]
+    print(f'{test=}')
+    print(f'{matrix[45]=}')
+    
     return matrix
 
+def generate_transition_matrix(movement_matrix):
+    for index in range(len(movement_matrix)):
+        summed_movements = 0
+        for destination_index in range(len(movement_matrix[index])):
+            summed_movements += movement_matrix[index][destination_index]
+        for destination_index in range(len(movement_matrix[index])):
+            movement_matrix[index][destination_index] = movement_matrix[index][destination_index]/summed_movements if summed_movements else 0
+    print(f'{movement_matrix[0]=}')
+    print(f'{movement_matrix[45]=}')
+    
+    return movement_matrix
+        
+
 def generate_shot_matrix(successful_shot_events, missed_shot_events):
-    shot_count_matrix = generate_base_matrix_shot()
-    shot_success_matrix = generate_base_matrix_shot()
+    shot_count_matrix = generate_base_matrix_2d()
+    shot_success_matrix = generate_base_matrix_2d()
 
     for shot_event in successful_shot_events:
         shot_x, shot_y = get_position_from_shot(shot_event)
@@ -118,14 +155,61 @@ def generate_shot_matrix(successful_shot_events, missed_shot_events):
 
     return shot_count_matrix, shot_success_matrix
 
+def generate_movement_probability_matrix(movement_transition_matrix, shot_matrix):
+    movement_probability_matrix = generate_base_matrix_2d()
+    for index in range(len(movement_probability_matrix)):
+        movement_sum = 0
+        for destination_index in range(len(movement_transition_matrix[index])):
+            movement_sum += movement_transition_matrix[index][destination_index]
+        movement_probability_matrix[index] = movement_sum/total_events if (total_events:=(movement_sum+shot_matrix[index])) else 0
+    return movement_probability_matrix
+
+def calculate_summed_xt(xt_matrix, index_to_calculate, transition_matrix):
+    summed_xt = 0
+    for index in range(len(xt_matrix[index_to_calculate])):
+        summed_xt += xt_matrix[index_to_calculate][index]*transition_matrix[index_to_calculate][index]
+
+
+def build_xt_matrix(competition_id: int, season_id: int):
+    pass_events, carry_events, successful_shot_events, missed_shot_events = get_events_sorted(competition_id, season_id)
+
+    movement_transition_matrix = generate_movement_matrix(pass_events, carry_events)
+
+    shot_matrix, conversion_matrix = generate_shot_matrix(successful_shot_events, missed_shot_events)
+
+    xt_base_matrix = generate_base_matrix_2d()
+
+    xt_matrix = generate_base_matrix_2d()
+
+    movement_probability_matrix = generate_movement_probability_matrix(movement_transition_matrix, shot_matrix)
+
+    transition_matrix = generate_transition_matrix(movement_transition_matrix)
+
+    print(f'{transition_matrix[42]=}')
+
+    for _ in range(10):
+        for index in range(len(xt_base_matrix)):
+            summed_xt = 0
+            for destination_index in range(len(transition_matrix[index])):
+                summed_xt += transition_matrix[index][destination_index] * xt_base_matrix[destination_index]
+            print(f'{summed_xt=}')
+            xt_matrix[index] = (1 - movement_probability_matrix[index]) * conversion_matrix[index] + movement_probability_matrix[index] * summed_xt
+            # xt_matrix[index] = (1 - movement_probability_matrix[index]) * conversion_matrix[index]
+        xt_base_matrix = xt_matrix
+
+    parsed_list = [xt_matrix[12*i:12*(i+1)] for i in range(8)]
+    # print(f'{parsed_list=}')
+
+    import matplotlib.pyplot as plt
+
+    plt.imshow(parsed_list, cmap='hot', interpolation='nearest')
+    plt.show()
+
+    return xt_matrix
 
 
 
 competition_id = 7
 season_id = 27
 
-pass_events, carry_events, successful_shot_events, missed_shot_events = get_events_sorted(competition_id, season_id)
-
-generate_movement_matrix(pass_events, carry_events)
-
-generate_shot_matrix(successful_shot_events, missed_shot_events)
+build_xt_matrix(competition_id, season_id)
