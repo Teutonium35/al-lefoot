e96d799de01a4c9c7aa79d37efdeebdf5c963f9b
MathieuH35 <teutonium35@gmail.com>
2024-11-08 09:57:04 +0100

    clean: add/remove prints

--- xt.py
+++ xt.py
@@ -11,23 +11,28 @@ WIDTH_SUBDIVISION_AMOUNT = int(FOOTBALL_FIELD_WIDTH/SUBDIVISION_SIZE)
 def get_events_sorted(competition_id: int, season_id: int):
     match_list = get_matches_from_season_and_competition(competition_id, season_id)
     match_events = []
+    total_length = len(match_list)
+    current_index = 0
     for match_id in match_list:
-        print(f"processing match no {match_id}")
+        current_index += 1
+        print(f"Retrieving events... {int(current_index/total_length*100)}%", end='\r')
         for new_event in get_events_from_match_id(match_id):
             match_events.append(new_event)
+    print('Finished retrieving events')
 
     pass_events = []
     carry_events = []
     successful_shot_events = []
     missed_shot_events = []
 
+    total_length = len(match_events)
+    current_index = 0
     for event in match_events:
+        current_index += 1
+        print(f"Sorting events... {int(current_index/total_length*100)}%", end='\r')
         try:
             if event["type"]["name"] == "Pass":
                 pass_events.append(event)
-                if event["location"][0]>120 or event["location"][1]>80:
-                    print(f"Event {event['id']}")
-                    exit(0)
             elif event["type"]["name"] == "Carry":
                 carry_events.append(event)
             elif event["type"]["name"] == "Shot":
@@ -38,15 +43,9 @@ def get_events_sorted(competition_id: int, season_id: int):
         except:
             print(f"Could not process event {event}")
 
-
-    print(f'{len(pass_events)=}')
-
-    print(f'{len(carry_events)=}')
-
-    print(f'{len(missed_shot_events)=}')
-
-    print(f'{len(successful_shot_events)=}')
-
+    print("Finished sorting events")
+    print(f'Retrieved {len(pass_events)} passes, {len(carry_events)} carries, {len(missed_shot_events)} missed shots and {len(successful_shot_events)} successful shots')
+    
     return pass_events, carry_events, successful_shot_events, missed_shot_events
 
 def get_array_position_from_field_indexes(length_index, width_index) -> int:
@@ -76,9 +75,6 @@ def get_position_from_shot(shot_event) -> Tuple[int, int]:
 
 def generate_base_matrix_3d():
     matrix = [[0]*LENGTH_SUBDIVISION_AMOUNT*WIDTH_SUBDIVISION_AMOUNT for _ in range(LENGTH_SUBDIVISION_AMOUNT*WIDTH_SUBDIVISION_AMOUNT)]
-    # print(f'{matrix=}')
-    # matrix[0][0] += 1
-    # print(f'{matrix=}')
     return matrix
 
 def generate_base_matrix_2d():
@@ -86,9 +82,6 @@ def generate_base_matrix_2d():
 
 def generate_movement_matrix(pass_events, carry_events):
     matrix = generate_base_matrix_3d()
-    # print(f'{matrix=}')
-    # print(f'{len(matrix)=}')
-    # print(f'{len(matrix[0])=}')
 
     for pass_event in pass_events:
         start_x, start_y, end_x, end_y = get_positions_from_pass(pass_event)
@@ -97,10 +90,6 @@ def generate_movement_matrix(pass_events, carry_events):
         start_array_index = get_array_position_from_field_indexes(start_lindex, start_windex)
         end_array_index = get_array_position_from_field_indexes(end_lindex, end_windex)
         matrix[start_array_index][end_array_index] += 1
-        # print(f'{matrix[start_array_index][end_array_index]=}')
-        # print(f'{start_array_index=}')
-        # print(f'{end_array_index=}')
-    # print(f'{matrix=}')
 
     for carry_event in carry_events:
         start_x, start_y, end_x, end_y = get_positions_from_carry(carry_event)
@@ -110,20 +99,6 @@ def generate_movement_matrix(pass_events, carry_events):
         end_array_index = get_array_position_from_field_indexes(end_lindex, end_windex)
         matrix[start_array_index][end_array_index] += 1
 
-    # print(f'{matrix[0]=}')
-    # test = 0
-    # for index in matrix[0]:
-    #     test += matrix[0][index]
-    # print(f'{test=}')
-    # print(f'{matrix[45]=}')
-    # matrix[0][0] +=1
-
-    # print(f'{matrix[0]=}')
-    # test = 0
-    # for index in matrix[0]:
-    #     test += matrix[0][index]
-    # print(f'{test=}')
-    # print(f'{matrix[45]=}')
     
     return matrix
 
@@ -134,8 +109,6 @@ def generate_transition_matrix(movement_matrix):
             summed_movements += movement_matrix[index][destination_index]
         for destination_index in range(len(movement_matrix[index])):
             movement_matrix[index][destination_index] = movement_matrix[index][destination_index]/summed_movements if summed_movements else 0
-    # print(f'{movement_matrix[0]=}')
-    # print(f'{movement_matrix[45]=}')
     
     return movement_matrix
 
@@ -149,11 +122,6 @@ def generate_shot_matrix(successful_shot_events, missed_shot_events):
         array_index = get_array_position_from_field_indexes(shot_lindex, shot_windex)
         shot_count_matrix[array_index] += 1
         shot_success_matrix[array_index] += 1
-        print(f'{shot_x=}')
-        print(f'{shot_y=}')
-        print(f'{shot_lindex=}')
-        print(f'{shot_windex=}')
-        print(f'{array_index=}')
 
     for shot_event in missed_shot_events:
         shot_x, shot_y = get_position_from_shot(shot_event)
@@ -163,8 +131,6 @@ def generate_shot_matrix(successful_shot_events, missed_shot_events):
     
     for index in range(len(shot_success_matrix)):
         shot_success_matrix[index] = shot_success_matrix[index]/shot_count_matrix[index] if shot_count_matrix[index] else 0
-    # print(f'{shot_count_matrix=}')
-    # print(f'{shot_success_matrix=}')
 
 
     plot_shots(successful_shot_events)
@@ -185,7 +151,6 @@ def plot_shots(shot_events):
     locations = []
     for event in shot_events:
         locations.append(event["location"])
-    print(f'{locations=}')
 
     import matplotlib.pyplot as plt
 
@@ -218,7 +183,6 @@ def build_xt_matrix(competition_id: int, season_id: int):
     shot_matrix, conversion_matrix = generate_shot_matrix(successful_shot_events, missed_shot_events)
 
     parsed_list = [shot_matrix[LENGTH_SUBDIVISION_AMOUNT*i:LENGTH_SUBDIVISION_AMOUNT*(i+1)] for i in range(WIDTH_SUBDIVISION_AMOUNT)]
-    # print(f'{parsed_list=}')
 
     import matplotlib.pyplot as plt
     plt.figure()
@@ -235,20 +199,16 @@ def build_xt_matrix(competition_id: int, season_id: int):
 
     transition_matrix = generate_transition_matrix(movement_transition_matrix)
 
-    # print(f'{transition_matrix[42]=}')
 
     for _ in range(10):
         for index in range(len(xt_base_matrix)):
             summed_xt = 0
             for destination_index in range(len(transition_matrix[index])):
                 summed_xt += transition_matrix[index][destination_index] * xt_base_matrix[destination_index]
-            # print(f'{summed_xt=}')
             xt_matrix[index] = (1 - movement_probability_matrix[index]) * conversion_matrix[index] + movement_probability_matrix[index] * summed_xt
-            # xt_matrix[index] = (1 - movement_probability_matrix[index]) * conversion_matrix[index]
         xt_base_matrix = xt_matrix
 
     parsed_list = [xt_matrix[LENGTH_SUBDIVISION_AMOUNT*i:LENGTH_SUBDIVISION_AMOUNT*(i+1)] for i in range(WIDTH_SUBDIVISION_AMOUNT)]
-    # print(f'{parsed_list=}')
 
     import matplotlib.pyplot as plt
     plt.figure()
